f23b1d349746813e86400f950dfdf55632505358
xGihyun <grdnnz.dev@proton.me>
2026-01-05 11:11:52 +0800

    feat(auth): implement function to get current session

--- src/lib/user/auth.ts
+++ src/lib/user/auth.ts
@@ -1,1 +1,20 @@
+import type { ApiResponse } from "../api";
+import { getCookie } from "../cookie";
+import type { AuthSession } from "./model";
+
 export const AUTH_TOKEN_KEY = "auth_token";
+
+export async function validateSession(): Promise<AuthSession | null> {
+	const token = getCookie(AUTH_TOKEN_KEY);
+	if (!token) return null;
+
+	const response = await fetch("/sessions", {
+		headers: { Authorization: `Bearer ${token}` },
+	});
+	if (!response.ok) {
+		return null;
+	}
+
+	const result: ApiResponse<AuthSession> = await response.json();
+	return result.data;
+}
