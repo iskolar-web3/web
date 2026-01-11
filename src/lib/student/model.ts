7ed7cddc39a5b91145bbc67a9c162b5c9b74b246
xGihyun <grdnnz.dev@proton.me>
2026-01-11 09:25:22 +0800

    refactor: move school and student types on `/lib`

--- src/lib/student/model.ts
+++ src/lib/student/model.ts
@@ -1,8 +1,12 @@
 import z from "zod";
 import { enumDetailSchema } from "../api";
-import { Gender } from "@/types/student";
 import { contactDetailSchema } from "../user/model";
 import { schoolSchema } from "../school/model";
+
+export enum Gender {
+	Male = "male",
+	Female = "female",
+}
 
 export const studentSchema = z.object({
 	id: z.uuidv4(),
