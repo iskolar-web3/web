9b0fd02d9762ecfc922a42051177a052137db4f0
xGihyun <grdnnz.dev@proton.me>
2026-01-13 10:29:38 +0800

    style(onboarding): format code

--- src/routes/_onboarding.tsx
+++ src/routes/_onboarding.tsx
@@ -3,32 +3,33 @@ import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
 import type { JSX } from "react";
 
 export const Route = createFileRoute("/_onboarding")({
-  component: OnboardingLayout,
-  beforeLoad: async ({context}) => {
+	component: OnboardingLayout,
+	beforeLoad: async ({ context }) => {
 		const session = await context.auth.getSession();
 		if (!session) {
 			return;
 		}
 
-        const path = getDefaultPathOfRole(session.user)
-        throw redirect({to: path})
-  }
+		const path = getDefaultPathOfRole(session.user);
+		throw redirect({ to: path });
+	},
 });
 
 function OnboardingLayout(): JSX.Element {
-  return (
-    <div 
-      className="min-h-screen flex items-center justify-center px-6"
-      style={{
-        backgroundImage: "url('/background.jpg')",
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat'
-      }}
-    >
-      <div className="w-full">
-        <Outlet />
-      </div>
-    </div>
-  );
+	return (
+		<div
+			className="min-h-screen flex items-center justify-center px-6"
+			style={{
+				backgroundImage: "url('/background.jpg')",
+				backgroundSize: "cover",
+				backgroundPosition: "center",
+				backgroundRepeat: "no-repeat",
+			}}
+		>
+			<div className="w-full">
+				<Outlet />
+			</div>
+		</div>
+	);
 }
+
