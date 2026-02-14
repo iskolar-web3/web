90be11fc03c1b2f4510479629cc131e9672d3a18
xGihyun <grdnnz.dev@proton.me>
2026-02-15 07:58:56 +0800

    style: format code

--- src/integrations/wagmi/provider.tsx
+++ src/integrations/wagmi/provider.tsx
@@ -1,20 +1,17 @@
-import { WagmiProvider as WagmiProviderBase } from 'wagmi';
-import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import '@rainbow-me/rainbowkit/styles.css';
-import { config } from '@/lib/wagmi';
-import { type ReactNode } from 'react';
+import { WagmiProvider as WagmiProviderBase } from "wagmi";
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import "@rainbow-me/rainbowkit/styles.css";
+import { config } from "@/lib/wagmi";
+import { type ReactNode } from "react";
 
 interface WagmiProviderProps {
-  children: ReactNode;
+	children: ReactNode;
 }
 
 export function WagmiProvider({ children }: WagmiProviderProps) {
-  return (
-    <WagmiProviderBase config={config}>
-        <RainbowKitProvider>
-          {children}
-        </RainbowKitProvider>
-    </WagmiProviderBase>
-  );
+	return (
+		<WagmiProviderBase config={config}>
+			<RainbowKitProvider>{children}</RainbowKitProvider>
+		</WagmiProviderBase>
+	);
 }
-
