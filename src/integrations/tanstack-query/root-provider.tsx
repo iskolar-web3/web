b33a41aeccc34da9a728c696ddcdf1af8e8ec39d
xGihyun <grdnnz.dev@proton.me>
2026-02-15 08:47:50 +0800

    perf: put values on query client's `defaultOptions` to prevent unneccessary refetches

--- src/integrations/tanstack-query/root-provider.tsx
+++ src/integrations/tanstack-query/root-provider.tsx
@@ -1,7 +1,18 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export function getContext() {
-	const queryClient = new QueryClient();
+	const queryClient = new QueryClient({
+		defaultOptions: {
+			queries: {
+				staleTime: 1 * 60 * 1000,
+				gcTime: 5 * 60 * 1000,
+				refetchOnWindowFocus: false,
+				refetchOnMount: false,
+				retry: 1,
+			},
+		},
+	});
+
 	return {
 		queryClient,
 	};
